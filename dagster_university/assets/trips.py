9e5b08b9d25f77f252b5a5a53e149265363e32fd
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-02-09 08:01:24 -0500

    add second asset: taxi_zones_file

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -15,3 +15,15 @@ def taxi_trips_file():
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@asset
+def taxi_zones_file():
+    """
+     The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+            f"https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+        )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
