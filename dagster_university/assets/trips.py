fc05dc05c578c7bfec99313db4e7956351085add
Gareth Brickman <50636644+garethbrickman@users.noreply.github.com>
2024-02-09 12:37:14 -0500

    add asset for loading taxi_trips dataset into DuckDB

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -1,4 +1,6 @@
 import requests
+import duckdb
+import os
 from . import constants
 from dagster import asset
 
@@ -27,3 +29,30 @@ def taxi_zones_file():
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
+
+@asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips():
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
